97e6d84fe422d6eb20a061fcb1e6a597677f40f5
Ain Ghazal <ainghazal42@gmail.com>
2022-11-08 15:37:50 +0100

    evaluate goccy/go-json in benchmark too
    choosing a more performant parsing library, just for the lulz.
    also note that compiling and executing the binary shaves some ms from
    the total runtime.

--- bench.sh
+++ bench.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-poetry run python bench_python.py && go run bench_go.go
+go get ./...
+go build bench_go.go
+go build bench_go_goccy.go
+poetry run python bench_python.py && ./bench_go && ./bench_go_goccy
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hellais/oonidata-bench
 
 go 1.18
+
+require github.com/goccy/go-json v0.9.11
